645bf846c9f56c017059b586a6fa56a2ba0743d4
DESKTOP-SNGU52T\User <vadympaiuk@gmail.com>
2023-08-15 00:03:31 +0300

    remove Gutenberg for pages

--- wp-content/themes/haltha/inc/settings.php
+++ wp-content/themes/haltha/inc/settings.php
@@ -40,3 +40,8 @@
 		
 		return $data;
 	}
+	
+	add_filter('use_block_editor_for_post', 'my_disable_gutenberg', 10, 2);
+	function my_disable_gutenberg( $can_edit, $post ){
+		return $post->post_type == 'post';
+	}
